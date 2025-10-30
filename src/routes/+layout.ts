849f6053be1d9081995954834e3a4bc3dade41a8
M-chan <new.jasjan.programmer@gmail.com>
2025-10-31 01:41:26 +0500

    some meta tag changes

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,27 +1,30 @@
 import type { LayoutLoad } from "./$types";
 
 export const load: LayoutLoad = () => {
-  return {
-    meta: {
-      title: {
-        default: "Abdulkhafizov07 — Developer",
-        template: "%s — Abdulkhafizov07",
-      },
-      description: "Middle-junior developer crafting modern web apps ⚡",
-      openGraph: {
-        title: "Abdulkhafizov07 — Web Developer",
-        image: "https://abdulkhafizov.uz/assets/social-preview.png",
-        description: "Middle-junior developer crafting modern web apps ⚡",
-        type: "website",
-        url: "https://abdulkhafizov.uz",
-      },
-      twitter: {
-        card: "summary_large_image",
-        creator: "@abdulkhafizov07",
-        title: "Abdulkhafizov07 — Web Developer",
-        description: "Middle-junior developer crafting modern web apps ⚡",
-        image: "https://abdulkhafizov.uz/assets/social-preview.png",
-      },
-    },
-  };
+    return {
+        meta: {
+            title: {
+                default: "Abdulkhafizov07 — Developer LL",
+                template: "%s — Abdulkhafizov07",
+            },
+            icon: "https://abdulkhafizov.uz/assets/favicon.ico",
+            description: "Middle-junior developer crafting modern web apps ⚡",
+            openGraph: {
+                title: "Abdulkhafizov07 — Web Developer",
+                image: "https://abdulkhafizov.uz/assets/social-preview.jpg",
+                description:
+                    "Middle-junior developer crafting modern web apps ⚡",
+                type: "website",
+                url: "https://abdulkhafizov.uz",
+            },
+            twitter: {
+                card: "summary_large_image",
+                creator: "@abdulkhafizov07",
+                title: "Abdulkhafizov07 — Web Developer",
+                description:
+                    "Middle-junior developer crafting modern web apps ⚡",
+                image: "https://abdulkhafizov.uz/assets/social-preview.jpg",
+            },
+        },
+    };
 };
