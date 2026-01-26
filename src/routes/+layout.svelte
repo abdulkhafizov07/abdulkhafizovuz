ed7ca26ad315f7f9f2ce2d458cfd8725e93621f4
Developer LL <new.jasjan.programmer@gmail.com>
2026-01-27 03:59:26 +0500

    test: added development notice and checking ci/cd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,10 @@
     <title>{_metadata.title}</title>
 </svelte:head>
 
+<div
+    class="fixed bottom-4 left-4 right-4 bg-black border-2 border-gray-400 p-4 text-white rounded-xl"
+>
+    <p>Under development. Please, check out next time</p>
+</div>
+
 <div class="w-full h-screen bg-black"><slot /></div>
